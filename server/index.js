228139c3660e55aea29b1c63da44c2ca68e58359
taonixiaoniao <211331569@qq.com>
2024-12-08 16:21:51 +0800

    删除无用代码
    feat：删除无用代码

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 io.on('connection', socket => {
-  logger.info('有位靓仔进入了聊天室')
+  logger.info('有位靓仔进入了聊天室：', socket)
   socket.on('disconnect', () => {
     logger.info('靓仔离开了聊天室')
   })
